470fd317f00ab51d70abf4bfd384c9a21f81707b
ieeedevops <110836015+nio2004@users.noreply.github.com>
2024-04-11 23:03:35 +0530

    Fixing Bugs

--- webcrat-clone/src/App.jsx
+++ webcrat-clone/src/App.jsx
@@ -81,7 +81,7 @@ function App() {
           })
           .catch(error => {
             console.error('Error fetching contributors:', error);
-            setMessage('Error fetching contributors:', error);
+            // setMessage('Error fetching contributors:', error);
           });
       // }
     }
